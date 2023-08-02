1dedeabe80fc97648304702300efbc36533d39f8
Horbunov Yurii <urijnet@gmail.com>
2023-08-02 08:14:16 +0300

    tasck-1-1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
 
 new SlimSelect({
   select: '.breed-select',
